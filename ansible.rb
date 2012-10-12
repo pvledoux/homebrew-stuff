4ebe150b63e1b0df96124760daba332d779ae5cc
Mark Nottingham <mnot@mnot.net>
2012-10-12 12:59:44 +1100

    use inreplace to change dist path

--- ansible.rb
+++ ansible.rb
@@ -12,6 +12,9 @@ class Ansible < Formula
   depends_on 'jinja2' => :python
 
   def install
-    system "/usr/local/bin/python setup.py install --prefix='#{prefix}' --exec-prefix='#{prefix}' --install-data=#{prefix}/share/ansible"
+    inreplace 'lib/ansible/constants.py' do |s| 
+      s.gsub! /\/usr\/share\/ansible\//, '/usr/local/Cellar/ansible/'
+    end
+    system "/usr/local/bin/python setup.py install --prefix='#{prefix}' --exec-prefix='#{prefix}'"
   end
 end
