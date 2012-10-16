b230662212230f29234a0229799d3f55799cf11f
Mark Nottingham <mnot@mnot.net>
2012-10-16 14:42:11 +1100

    remove commented deps; move ansible tasks to share not cellar.

--- ansible.rb
+++ ansible.rb
@@ -4,16 +4,13 @@ class Ansible < Formula
   homepage 'http://ansible.github.com/'
   head 'https://github.com/ansible/ansible.git', :using => :git
 
-#  depends_on 'asciidoc'
-#  depends_on 'python'
-#  depends_on 'distutils2' => :python
   depends_on 'paramiko' => :python
-#  depends_on 'pyyaml' => :python
+  depends_on 'pyyaml' => :python
   depends_on 'jinja2' => :python
 
   def install
     inreplace 'lib/ansible/constants.py' do |s| 
-      s.gsub! /\/usr\/share\/ansible\//, '/usr/local/Cellar/ansible/'
+      s.gsub! /\/usr\/share\/ansible\//, '/usr/local/share/ansible/'
     end
     system "/usr/local/bin/python setup.py install --prefix='#{prefix}' --exec-prefix='#{prefix}'"
   end
