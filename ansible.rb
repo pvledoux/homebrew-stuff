5ff5a18254330cce19ff19a3633c1377a24440d5
Mark Nottingham <mnot@mnot.net>
2012-10-18 10:42:52 +1100

    add head url, sha1

--- ansible.rb
+++ ansible.rb
@@ -3,6 +3,8 @@ require 'formula'
 class Ansible < Formula
   homepage 'http://ansible.github.com/'
   head 'https://github.com/ansible/ansible.git', :using => :git
+  url 'https://github.com/downloads/ansible/ansible/ansible-0.7.1.tar.gz'
+  sha1 'b3128b13728b0041fdac848a29e9387d89f10c41'
 
   depends_on 'paramiko' => :python
 #  depends_on 'pyyaml' => :python
