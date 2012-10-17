4ce00538b5849830e8125dbf88fdae083628c082
Mark Nottingham <mnot@mnot.net>
2012-10-18 10:51:38 +1100

    require python; try different gsub

--- ansible.rb
+++ ansible.rb
@@ -6,13 +6,14 @@ class Ansible < Formula
   url 'https://github.com/downloads/ansible/ansible/ansible-0.7.1.tar.gz'
   sha1 'b3128b13728b0041fdac848a29e9387d89f10c41'
 
+  depends_on 'python'
   depends_on 'paramiko' => :python
-#  depends_on 'pyyaml' => :python
   depends_on 'jinja2' => :python
+#  depends_on 'pyyaml' => :python
 
   def install
     inreplace 'lib/ansible/constants.py' do |s| 
-      s.gsub! /\/usr\/share\/ansible\//, '/usr/local/share/ansible/'
+      s.gsub! '/usr/share/ansible/', '/usr/local/share/ansible/'
     end
     
     system "/usr/local/bin/python setup.py install --prefix='#{prefix}' --exec-prefix='#{prefix}'"
