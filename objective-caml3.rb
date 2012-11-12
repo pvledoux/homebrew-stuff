9e6d9606e94007b97414738dd5bb710bff1e09d2
Mark Nottingham <mnot@mnot.net>
2012-11-12 11:41:57 +1100

    add unison240, objective-caml3

---
+++ objective-caml3.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class ObjectiveCaml < Formula
+  homepage 'http://www.ocaml-lang.org'
+  url 'http://caml.inria.fr/pub/distrib/ocaml-3.12/ocaml-3.12.1.tar.gz'
+  sha1 '05125da055d39dd6fe8fe5c0155b2e9f55c10dfd'
+
+  depends_on :x11 if MacOS::X11.installed?
+
+  def install
+    system "./configure", "--prefix", HOMEBREW_PREFIX,
+                          "--mandir", man,
+                          "-cc", ENV.cc,
+                          "-with-debug-runtime",
+                          "-aspp", "#{ENV.cc} -c"
+    ENV.deparallelize # Builds are not parallel-safe, esp. with many cores
+    system "make world"
+    system "make opt"
+    system "make opt.opt"
+    system "make", "PREFIX=#{prefix}", "install"
+
+    # site-lib in the Cellar will be a symlink to the HOMEBREW_PREFIX location,
+    # which is mkpath'd by Keg#link when something installs into it
+    ln_s HOMEBREW_PREFIX/"lib/ocaml/site-lib", lib/"ocaml/site-lib"
+  end
+end
