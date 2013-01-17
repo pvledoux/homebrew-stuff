bbee687b15fd96adf18bb5be1aea0ed8f4126eec
Mark Nottingham <mnot@mnot.net>
2013-01-17 20:07:23 +1100

    Update ansible to 0.9

--- ansible.rb
+++ ansible.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Ansible < Formula
   homepage 'http://ansible.github.com/'
   head 'https://github.com/ansible/ansible.git', :using => :git
-  url 'https://github.com/downloads/ansible/ansible/ansible-0.7.1.tar.gz'
-  sha1 'b3128b13728b0041fdac848a29e9387d89f10c41'
+  url 'https://github.com/ansible/ansible/archive/v0.9.tar.gz'
+  sha1 '23c3b807dcf296002663c10b10c42c3885f1340f'
 
   depends_on 'python'
   depends_on 'paramiko' => :python
